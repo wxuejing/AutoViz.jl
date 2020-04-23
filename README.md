91a6b57949f5d839bda55ad18d81667d6930da09
Xuejing Wang <xuejing@Xuejings-MacBook-Pro.local>
2020-04-22 21:47:45 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# AutoViz
+
+This package provides visualization tools for [AutomotiveDrivingModels](https://github.com/sisl/AutomotiveDrivingModels.jl).
+Built off of PGFPlots and Cairo.
+
+You can find the [documentation here](http://nbviewer.ipython.org/github/sisl/AutoViz.jl/blob/master/doc/AutoViz.ipynb).
+
+[![Build Status](https://travis-ci.org/sisl/AutoViz.jl.svg?branch=master)](https://travis-ci.org/sisl/AutoViz.jl)
+[![Coverage Status](https://coveralls.io/repos/sisl/AutoViz.jl/badge.svg)](https://coveralls.io/r/sisl/AutoViz.jl)
+
+![AutoViz](readmeimage.png)
---
+++ src/AutoViz.jl
@@ -0,0 +1,41 @@
+__precompile__(true)
+
+module AutoViz
+
+using Reexport
+using Parameters
+using AutomotiveDrivingModels
+
+@reexport using Colors
+@reexport using Cairo
+
+import Reel
+Reel.set_output_type("gif")
+
+# using Reactive
+# import Gtk
+# using PGFPlots
+
+export
+        DEFAULT_CANVAS_WIDTH,
+        DEFAULT_CANVAS_HEIGHT,
+        render!,
+        get_pastel_car_colors
+
+const DEFAULT_CANVAS_WIDTH = 1000
+const DEFAULT_CANVAS_HEIGHT = 600
+
+include("colorscheme.jl")
+include("rendermodels.jl")
+
+include("cameras.jl")
+include("interface.jl")
+include("overlays.jl")
+include("reel_drive.jl")
+
+include("1d/main.jl")
+include("2d/main.jl")
+
+
+
+end # module
